43c3cf2802c22b599e821d519d2c9ef45570e62f
syouzan420 <syouzanponkikki@yahoo.co.jp>
2023-12-03 22:31:30 +0900

    update

--- src/MySDL/Draw.hs
+++ src/MySDL/Draw.hs
@@ -1,8 +1,15 @@
-module MySDL.Draw (initDraw) where
+module MySDL.Draw (initDraw,draw) where
 
 import SDL.Video (Renderer)
-import SDL.Video.Renderer (rendererDrawColor,clear)
+import SDL.Video.Renderer (Surface,Texture,SurfacePixelFormat(..),Rectangle(..),PixelFormat(..)
+                          ,rendererDrawColor,clear,destroyTexture
+                          ,createTextureFromSurface,copyEx,present
+                          ,lockSurface,unlockSurface,surfacePixels,surfaceFormat,createRGBSurfaceFrom)
 import SDL (($=))
+import SDL.Vect (Point(P),V2(..))
+import Foreign.Ptr (Ptr(..),castPtr)
+import Foreign.ForeignPtr (newForeignPtr_)
+import Data.Vector.Storable.Mutable (unsafeFromForeignPtr0)
 import Control.Monad.IO.Class (MonadIO)
 import MyData (backColor)
 
@@ -10,3 +17,27 @@ initDraw :: MonadIO m => Renderer -> m ()
 initDraw re = do
   rendererDrawColor re $= backColor
   clear re
+
+draw :: Renderer -> [Surface] -> IO ()
+draw re imageS = do
+  imageTO <- mapM (createTextureFromSurface re) imageS
+  let imageS0 = head imageS
+  lockSurface imageS0
+  let pixelFormat = RGBA8888 
+  pointer0 <- surfacePixels imageS0
+  frPointer <- newForeignPtr_ (castPtr pointer0)
+  let mvector = unsafeFromForeignPtr0 frPointer (64*64)
+  newImageS0 <- createRGBSurfaceFrom mvector (V2 64 64) 64 pixelFormat
+  unlockSurface imageS0
+  initDraw re
+  imageDraw re imageTO
+  mapM_ destroyTexture imageTO
+  present re
+
+imageDraw :: Renderer -> [Texture] -> IO ()
+imageDraw re imageT = 
+  mapM_ (\x -> copyEx re (imageT!!x) (Just (Rectangle (P (V2 0 0)) (V2 64 64)))
+                                     (Just (Rectangle (P (V2 (fromIntegral x*70+100) 100)) (V2 64 64)))
+                                     0 Nothing (V2 False False)) [0..(length imageT-1)]
+
+
--- src/MySDL/Loop.hs
+++ src/MySDL/Loop.hs
@@ -4,6 +4,7 @@ import Control.Monad.State.Strict (StateT,get,put)
 import Control.Monad.IO.Class (liftIO)
 import SDL.Video.Renderer (Renderer,Surface)
 import SDL.Time (delay)
+import MySDL.Draw (draw)
 import MyData (World(..), Input(..),  delayTime)
 import Event (inputEvent)
 
@@ -12,7 +13,8 @@ loop re imageS = do
   inp <- liftIO inputEvent
   wld <- get
   let newWorld = wld{abc=abc wld + 1}
-  liftIO (print (abc wld))
+  --liftIO (print (abc wld))
   put newWorld
+  liftIO $ draw re imageS
   delay delayTime
   if inp==QIT then return () else loop re imageS
